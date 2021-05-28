67aab1311d5568764a3cef4789f48fb62019cdcc
John <jmacpherson@homex.com>
2021-05-28 10:30:20 -0400

    fix - handling of channel `type` since it always appears as PUBLIC before it is fully synchronized

--- lib/src/channel.dart
+++ lib/src/channel.dart
@@ -467,10 +467,12 @@ class Channel {
       _attributes = Attributes.fromMap(map['attributes'].cast<String, dynamic>());
     }
 
-if (map['type'] != null) {
-  final type = EnumToString.fromString(ChannelType.values, map['type']);
-  _type = type != null ? type : _type;
-}
+    if (map['type'] != null) {
+      final type = EnumToString.fromString(ChannelType.values, map['type']);
+      if (type != null) {
+        _type = type;
+      }
+    }
 
     _status = EnumToString.fromString(ChannelStatus.values, map['status']) ?? ChannelStatus.UNKNOWN;
 
